ec9c13ba041dca7365967c1d204531f7d0baf3cf
Guilherme Araujo <guilherme1984.araujo@gmail.com>
2022-11-15 20:40:26 -0300

    correcoes necessarias

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react"
+import axios from "axios"
+import { useEffect, useState } from "react"
 import DatePicker from "react-datepicker"
 import "react-datepicker/dist/react-datepicker.css"
 
@@ -10,10 +11,17 @@ import './style.css'
 function SalesCard() {
 
     const min = new Date(new Date().setDate(new Date().getDate() - 365))
-    const max = new Date();
+    const max = new Date()
 
     const [minDate, setMinDate] = useState(min)
     const [maxDate, setMaxDate] = useState(max)
+
+    useEffect(() => {
+        axios.get("https://localhost:8080/sales")
+            .then(response => {
+                console.log(response.data)
+            })
+    }, [])
 
     return (
         <div className="dsmeta-card">
@@ -39,52 +47,57 @@ function SalesCard() {
             <table className="dsmeta-sales-table">
                 <thead>
                     <tr>
-                        <th className="s992">ID</th>
-                        <th className="s576">Data</th>
-                        <th>Vendedor</th>
-                        <th className="s992">Visitas</th>
-                        <th className="s992">Vendas</th>
-                        <th>Total</th>
-                        <th>Notificar</th>
+                        <td className="s992">ID</td>
+                        <td className="s576">Data</td>
+                        <td>Vendedor</td>
+                        <td className="s992">Visitas</td>
+                        <td className="s992">Vendas</td>
+                        <td>Total</td>
+                        <td>Notificar</td>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
-                        <th className="s992">#341</th>
-                        <th className="s576">08/07/2022</th>
-                        <th>Anakin</th>
-                        <th className="s992">15</th>
-                        <th className="s992">11</th>
-                        <th>R$ 55300.00</th>
-                        <th>
+                        <td className="s992">#341</td>
+                        <td className="s576">08/07/2022</td>
+                        <td>Anakin</td>
+                        <td className="s992">15</td>
+                        <td className="s992">11</td>
+                        <td>R$ 55300.00</td>
+                        <tr>
                             <div className="dsmeta-red-btn-container">
                                 <NotificationButton />
                             </div>
-                        </th>
+                        </tr>
                     </tr>
                     <tr>
-                        <th className="s992">#341</th>
-                        <th className="s576">08/07/2022</th>
-                        <th>Anakin</th>
-                        <th className="s992">15</th>
-                        <th className="s992">11</th>
-                        <th>R$ 55300.00</th>
+                        <td className="s992">#341</td>
+                        <td className="s576">08/07/2022</td>
+                        <td>Anakin</td>
 
-                        <div className="dsmeta-red-btn-container">
-                            <NotificationButton />
-                        </div>
+                        <td className="s992">15</td>
+                        <td className="s992">11</td>
+                        <td>R$ 55300.00</td>
+
+                        <tr>
+                            <div className="dsmeta-red-btn-container">
+                                <NotificationButton />
+                            </div>
+                        </tr>
                     </tr>
                     <tr>
-                        <th className="s992">#341</th>
-                        <th className="s576">08/07/2022</th>
-                        <th>Anakin</th>
-                        <th className="s992">15</th>
-                        <th className="s992">11</th>
-                        <th>R$ 55300.00</th>
+                        <td className="s992">#341</td>
+                        <td className="s576">08/07/2022</td>
+                        <td>Anakin</td>
+                        <td className="s992">15</td>
+                        <td className="s992">11</td>
+                        <td>R$ 55300.00</td>
 
-                        <div className="dsmeta-red-btn-container">
-                            <NotificationButton />
-                        </div>
+                        <tr>
+                            <div className="dsmeta-red-btn-container">
+                                <NotificationButton />
+                            </div>
+                        </tr>
                     </tr>
                 </tbody>
             </table>
